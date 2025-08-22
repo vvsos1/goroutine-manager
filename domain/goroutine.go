e966aaf16eff9b28f947d7ecaa8f75a787728547
박명규 <vvsos1@outlook.com>
2025-08-22 18:03:50 +0900

    고루틴, 고루틴 매니저 구현

--- domain/goroutine.go
+++ domain/goroutine.go
@@ -1,34 +1,111 @@
 package domain
 
-import "time"
+import (
+	"time"
+)
+
+type GoroutineId int
 
 type Goroutine struct {
-	ID           int64
+	Id           GoroutineId
 	SaveDuration time.Duration
-	repository   KeyValueRepository[string, string]
-	channel      chan struct{}
+	repository   KeyValueRepository[GoroutineId, string]
+	channel      chan interface{}
 	started      bool
 }
 
-func (g *Goroutine) Start() {
+func (g *Goroutine) StartInGoroutine() {
 	if g.started {
+		return
+	}
+	go g.process()
+}
+
+func (g *Goroutine) process() {
+	g.started = true
+	defer func() { g.started = false }()
+	tick := time.Tick(g.SaveDuration)
+	for {
+		select {
+		case operation := <-g.channel:
+			switch op := operation.(type) {
+			case *ReadOperation:
+				value := g.readFromRepository()
+				g.channel <- value
+			case *DeleteOperation:
+				return
+			case *UpdateOperation:
+				g.SaveDuration = op.SaveDuration
+				tick = time.Tick(g.SaveDuration)
+			}
+		case <-tick:
+			g.saveToRepository()
+		}
+	}
+}
+
+func (g *Goroutine) Read() string {
+	operation := &ReadOperation{}
+	g.channel <- operation
+	value := <-g.channel
+	if strValue, ok := value.(string); ok {
+		return strValue
+	}
+	// TODO: 에러 처리
+	return ""
+}
+
+func (g *Goroutine) Update(duration time.Duration) {
+	operation := &UpdateOperation{SaveDuration: duration}
+	g.channel <- operation
+}
+
+func (g *Goroutine) Delete() {
+	operation := &DeleteOperation{}
+	g.channel <- operation
+	close(g.channel) // 채널을 닫아 고루틴이 종료되도록 함
+}
+
+func (g *Goroutine) saveToRepository() {
+	err := g.repository.Put(g.Id, time.Now().String())
+	if err != nil {
+		// TODO: 에러 처리
 		return
 	}
 }
 
-var nextId int64 = 1
+func (g *Goroutine) readFromRepository() string {
+	value, err := g.repository.Get(g.Id)
+	if err != nil {
+		// TODO: 에러 처리
+		return ""
+	}
+	return value
+}
 
-func nextGoroutineId() int64 {
+var nextId GoroutineId = 1
+
+func nextGoroutineId() GoroutineId {
 	nextId++
 	return nextId
 }
 
-func NewGoroutine(duration time.Duration, repository KeyValueRepository[string, string]) Goroutine {
+func NewGoroutine(duration time.Duration, repository KeyValueRepository[GoroutineId, string]) Goroutine {
 	return Goroutine{
-		ID:           nextGoroutineId(),
+		Id:           nextGoroutineId(),
 		SaveDuration: duration,
 		repository:   repository,
-		channel:      make(chan struct{}),
+		channel:      make(chan interface{}),
 		started:      false,
 	}
+}
+
+type UpdateOperation struct {
+	SaveDuration time.Duration
+}
+
+type ReadOperation struct {
+}
+
+type DeleteOperation struct {
 }
--- domain/goroutine_manager.go
+++ domain/goroutine_manager.go
@@ -1,22 +1,58 @@
 package domain
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type GoroutineManager struct {
-	goroutines map[int64]Goroutine
-	repository KeyValueRepository[string, string]
+	goroutines map[GoroutineId]Goroutine
+	repository KeyValueRepository[GoroutineId, string]
 }
 
 func NewGoroutineManager() *GoroutineManager {
 	return &GoroutineManager{
-		goroutines: make(map[int64]Goroutine),
+		goroutines: make(map[GoroutineId]Goroutine),
 	}
 }
 
 func (gm *GoroutineManager) Create(req CreateGoroutine) (Goroutine, error) {
-	goroutine := NewGoroutine(req.SaveDuration)
-	gm.goroutines[goroutine.ID] = goroutine
+	goroutine := NewGoroutine(req.SaveDuration, gm.repository)
+	gm.goroutines[goroutine.Id] = goroutine
+
+	goroutine.StartInGoroutine()
+
 	return goroutine, nil
+}
+
+func (gm *GoroutineManager) Get(id GoroutineId) (string, error) {
+	goroutine, exists := gm.goroutines[id]
+	if !exists {
+		return "", fmt.Errorf("goroutine with id %d not found", id)
+	}
+	value := goroutine.Read()
+	return value, nil
+}
+
+func (gm *GoroutineManager) Update(id GoroutineId, duration time.Duration) error {
+	goroutine, exists := gm.goroutines[id]
+	if !exists {
+		return fmt.Errorf("goroutine with id %d not found", id)
+	}
+	goroutine.Update(duration)
+	return nil
+}
+
+func (gm *GoroutineManager) Delete(id GoroutineId) {
+	goroutine, exists := gm.goroutines[id]
+	if !exists {
+	}
+	goroutine.Delete()
+	delete(gm.goroutines, id)
+}
+
+func (gm *GoroutineManager) Count() int {
+	return len(gm.goroutines)
 }
 
 // 고루틴 생성 DTO
